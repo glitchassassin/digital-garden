782ebd2310e2f44a98ee3b96cfaa192daf494d77
Jon Winsley <jon.winsley@gmail.com>
2022-10-26 23:04:38 -0400

    setting relative url path

--- .eleventy.js
+++ .eleventy.js
@@ -18,7 +18,7 @@ module.exports = function (eleventyConfig) {
     })
         .use(require('markdown-it-footnote'))
         .use(require('markdown-it-attrs'))
-        .use(require('markdown-it-obsidian-images')())
+        .use(require('markdown-it-obsidian-images')({ relativeBaseURL: '../' }))
         .use(function (md) {
             // Recognize Mediawiki links ([[text]])
             md.linkify.add("[[", {
